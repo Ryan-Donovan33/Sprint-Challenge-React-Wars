72ab15cd3f64ca586d26dac60436f3456b3b1535
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-10 21:02:10 -0500

    styling card with styled components

--- starwars/src/components/Card/Card.js
+++ starwars/src/components/Card/Card.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 30%;
+	align-items: center;
+	margin: 3%;
+	background-color: #fec196;
+`;
+
+const Name = styled.h1`font-size: 1.2rem;`;
 
 const Card = (props) => {
 	return (
-		<div>
-			<p>Name: {props.jedi.name}</p>
+		<CardWrapper>
+			<Name>Name: {props.jedi.name}</Name>
 			<p>Height: {props.jedi.height}</p>
 			<p>Mass: {props.jedi.mass}</p>
 			<p>Hair Color: {props.jedi.hair_color}</p>
 			<p>Skin Color: {props.jedi.skin_color}</p>
 			<p>Eye Color: {props.jedi.eye_color}</p>
 			<p>Birth Year: {props.jedi.birth_year}</p>
-			<p>Gender: {props.jedi.gender}</p>
-		</div>
+		</CardWrapper>
 	);
 };
 
